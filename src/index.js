24aa254ec181a85b1ce7a17471d39ae8d1de1f8d
alec-petros <a.petros@verizon.net>
2018-04-10 13:22:33 -0400

    iterates through pokemans

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+  const pokemonArray = [];
+  const searchForm = document.getElementById('pokemon-search-form')
+
+  searchForm.addEventListener('keyup', e => {
+    alert(e.target.value)
+  })
+
+  class Pokemon {
+    constructor(height, weight, order, name, abilities, moves, stats, types, sprites) {
+      this.height = height;
+      this.weight = weight;
+      this.order = order;
+      this.name = name;
+      this.abilities = abilities;
+      this.moves = moves;
+      this.stats = stats;
+      this.types = types;
+      this.sprites = sprites;
+      pokemonArray.push(this);
+    }
+  }
+
+  pokemon["pokemons"].map(poke => new Pokemon(
+    poke["height"],
+    poke["weight"],
+    poke["order"],
+    poke["name"],
+    poke["abilities"],
+    poke["moves"],
+    poke["stats"],
+    poke["types"],
+    poke["sprites"],
+  ))
+
+  console.log(pokemonArray);
 })
